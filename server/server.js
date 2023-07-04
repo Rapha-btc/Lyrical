2a41e993e5beb6810d04d20e650cd310452ff6da
Rapha-btc <rapha@fontainebleau-management.com>
2023-07-04 19:48:19 +0000

    Cannot read properties of undefined (reading 'filter') at Object.parser

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+// const MONGO_URI = 'mongodb+srv://raphisidor:4o8eyDnuGnZdbU7I@cluster0.b2u6dqt.mongodb.net/';
+// const MONGO_URI = 'mongodb+srv://raphisidor:PWjlsJSmyzbIB1lt@cluster111.jxgzeow.mongodb.net/' ;
+const MONGO_URI = 'mongodb+srv://raphisidor:rapha@cluster111.jxgzeow.mongodb.net/?retryWrites=true&w=majority' ;
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
